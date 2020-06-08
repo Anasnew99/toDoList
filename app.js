42880647d911cb8ced07c8a10f8da2eadd86fef1
Anas Aneeque <anasnew99@gmail.com>
2020-06-08 12:24:17 +0530

    Added dates and users working properly

--- app.js
+++ app.js
@@ -15,13 +15,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 // Databases Creation
-
 mongoose.connect('mongodb://localhost:27017/toDoListDb',{useNewUrlParser:true,useUnifiedTopology:true});
-const itemsSchema = mongoose.Schema({items:[String]});
-const userSchema = mongoose.Schema({date:Date,itemCount:{type:Number,default:0},items:itemSchema});
-const appdb = mongoose.Schema({userEmail:String,noOfDates:{type:Number,default:0},userData:[userSchema]});
-const appdbModel = mongoose.model('User',appdb);
-
+const itemsSchema = mongoose.Schema({
+  user_id:String,
+  date_id:String,
+  item:String
+});
+const item = mongoose.model("Item",itemsSchema);
+const dateSchema = mongoose.Schema({
+  user_id:String,
+  date:Date
+});
+const userDate = mongoose.model("Date",dateSchema);
+const userSchema = mongoose.Schema({
+  userEmail:String
+});
+const user = mongoose.model("User",userSchema);
 // Route Handlers And App Logic
 
 /* Home Routes GET And POSt */
@@ -30,75 +39,94 @@ app.get("/", function(req, res) {
 });
 app.post("/", function(req, res){
   userEmail = req.body.userEmail;
-    // Logging to that user's Route.
-        res.redirect('/'+userEmail);
+  var user_id = 0;
+  user.findOne({userEmail:userEmail},function(err,data){
+    if(!err){
+      if(!data){
+        user.insertMany([{userEmail:userEmail}],function(err,data){
+          if(!err){
+            console.log("Inserted Successfully"+data.userEmail);
+            user_id = data._id;
+            res.redirect('/user/'+user_id);
+          }else{
+            console.log("Error");
+          }
+        });
+      }
+      else{
+        user_id=data._id;
+        console.log(data._id);
+        res.redirect('/user/'+user_id);
+      }
+    }
+    else{
+      console.log("Error");
+    }
+  });
+      // Logging to that user's Route.
+
 });
 
 
 /* User's Route Handler */
-app.get("/:userEmail", function(req,res){
+app.get("/user/:user_id", function(req,res){
   //Checking User in Database.
-      appdbModel.find({userEmail:req.params.userEmail},function(err, data){
-        if(!err){
-          if(!data){ // If User exists
-            res.render('date_entry',{data:data});
-          }
-          else{ //If User doesnot exist creating a new user.
-            appdbModel.insertOne({userEmail:req.params.userEmail},function(err,data){
-              if(!err){
-                console.log("Inserted Successfully");
-                res.redirect('/'+req.params.userEmail);
-              }
-            });
-          }
+    userDate.find({user_id:req.params.user_id},function(err,data){
+      if(!err){
+        listOfDate = [];
+        listOfId = [];
+        forwardLink = [];
+        data.forEach(function(date){
+          listOfDate.push(date.date);
+          listOfId.push(date._id);
+          forwardLink.push("/user/"+req.params.user_id+"/"+date._id);
+        });
+        content = {
+          title:"List of Your Dates",
+          list : listOfDate,
+          idList: listOfId,
+          type:"date",
+          user_id:req.params.user_id,
+          forwardLink:forwardLink
         }
-      });
-});
-app.post("/:userEmail",function(req,res){
-  date = req.body.date;
-  appdbModel.findOneAndUpdate({userEmail:req.params.userEmail},{$set:{userData[noOfDates].date:date,$inc:{noOfDates:1}},function(err,result){
-
-    if(!err){
-      console.log('Successfully Added Date');
-      res.redirect('/'+req.params.userEmail);
-    }
-    else{
-      console.log(err);
-    }
-  });
-});
-
-/*  Handling Email & Date Data Routes */
-
-app.get('/:userEmail/:count',function(req,res){
-  appdbModel.find({userEmail:req.params.userEmail},function(err,data){
-    if(!err){
-      if(!data.userData[count]){
-        toDoList = data.userData[count].items;
-        date = data.userData[count].date;
-        res.render('list',{toDoList:toDoList,date:date,userEmail:data.userEmail,count:req.params.count});
+        res.render('list',{content:content});
+        console.log("No Error");
       }
       else{
-        console.log('No Data Exist');
+        console.log("Error");
+      }
+    });
+});
+
+app.post('/create',function(req,res){
+  userDate.insertMany([{user_id:req.body.user_id,date:req.body.newItem}],function(err,data){
+    if(!err){
+      if(data){
+        console.log(data);
+        res.redirect('/user/'+req.body.user_id);
       }
     }
     else{
-      console.log(err);
+      console.log("error");
     }
   });
 });
 
-app.post('/:userEmail/:count',function(req,res){
-  item = req.body.item;
-  appdbModel.findOneAndUpdate({userEmail:req.params.userEmail},{$set:{userData[req.params.count].items[itemCount]:item,$inc:{userData[req.params.count].itemCount:1}}},function(err,result){
-    if(!err){
-      console.log(result);
-    }
-    else{
-      console.log(err);
-    }
-  });
+app.post('/delete',function(req,res){
+  if(req.body.datatype==='date'){
+    userDate.deleteOne({_id:req.body.dataid},function(err,response){
+      if(!err){
+        console.log("Data scuccefully deleted Redirecting....");
+        if(response){res.redirect('user/'+req.body.user_id);}
+        else{console.log("No data found");}
+      }
+      else{
+        console.log("error");
+      }
+    });
+  }
 });
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
